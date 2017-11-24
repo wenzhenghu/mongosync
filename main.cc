36161725b733669dd0216287d9884337ead9a65e
hzwenzhh <hzwenzhh@corp.netease.com>
2017-11-24 10:39:22 +0800

    delete shards handle in is_mongos mode.

--- main.cc
+++ main.cc
@@ -38,79 +38,25 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-  	// mongos -> mongos
+	// Connect to src mongodb instance/cluster 
+	MongoSync *mongosync = MongoSync::NewMongoSync(&opt);
+	if (!mongosync) {
+		LOG(FATAL) << "Create mongosync instance failed" << std::endl;
+		return -1;
+	}
+
   	if (opt.is_mongos) {
-    	if (opt.shard_user.empty() || opt.shard_passwd.empty()) {
-      		LOG(FATAL) << "Shard username or password should not be empty when src is mongos\n" << std::endl;
-      		return -1;
-    	}
-		
-    	// Get mongos shards host info
-    	MongoSync *mongosync = MongoSync::NewMongoSync(&opt);
-    	if (!mongosync) {
-      		LOG(FATAL) << "Create mongosync instance failed" << std::endl;
-      		return -1;
-    	}
 		
     	std::vector<std::string> shards = mongosync->GetShards();
     	if (mongosync->IsBalancerRunning()) {
       		LOG(FATAL) << "Balancer is running" << std::endl;
+			delete mongosync;
       		return -1;
-    	}
-		
-    	delete mongosync;
+    	}	
+  	} 
 
-    	// Create connection between shard and dst mongos
-    	int ret;
-    	pthread_t tid;
-    	std::vector<pthread_t> tids;
-    	std::vector<std::string> shard_ips;
-    	Options shard_opt(opt);
-    	shard_opt.src_user = opt.shard_user;
-    	shard_opt.src_passwd = opt.shard_passwd;
-
-		for (int i = 0; i < shards.size(); i++) {
-      		size_t slash_pos = shards[i].find('/');
-      		std::string shard_addr = shards[i].substr(slash_pos + 1);
-      		util::Split(shard_addr, ',', shard_ips);
-
-      		// find a SECONDARY src mongodb
-      		MongoSync *mongosync = NULL;
-      		int j = 0;
-      		for (j = 0; j < shard_ips.size(); j++) {
-        		shard_opt.src_ip_port = shard_ips[j];
-        		mongosync = MongoSync::NewMongoSync(&shard_opt); // delete in thread
-        		if (mongosync && !mongosync->IsMasterMongo())
-          			break;
-        		else if (mongosync && j == shard_ips.size() - 1)
-          			break;
-        		else
-          			delete mongosync;
-      		}
-			
-      		if (!mongosync) {
-        		LOG(FATAL) << "Create shard mongosync instance failed" << std::endl;
-        		return -1;
-      		}
-
-      		ret = pthread_create(&tid, NULL, mongo_to_mongos, (void *)mongosync);
-      		if (ret != 0)
-        		return -1;
-      		tids.push_back(tid);
-    	}
-
-    	for (int i = 0; i < tids.size(); i++) {
-      		pthread_join(tids[i], NULL);
-    	}
-  	} else {
-    	// Others sync methods
-    	MongoSync *mongosync = MongoSync::NewMongoSync(&opt);
-    	if (!mongosync) {
-      		return -1;
-    	}
-    	mongosync->Process();
-    	delete mongosync;
-  	}
+	mongosync->Process();
+    delete mongosync;
 
   	return 0;
 }
